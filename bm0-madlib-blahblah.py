9a9fcb169efdb94ae5c4db1801f3c79bbe36f95a
sebastianb120 <sebastianb120@nycstudents.net>
2025-12-01 14:51:11 -0500

    Create bm0-madlib-blahblah.py

---
+++ bm0-madlib-blahblah.py
@@ -0,0 +1,23 @@
+import random
+from random import randint
+
+
+print("adjective:")
+adj1 = input()
+
+print("adjective:")
+adj2 = input()
+
+print( "a place: ")
+place = input()
+
+print( "a verb ending in ING: ")
+verb = input()
+
+storynum = random.randint(0,1)
+
+if storynum == 1:
+	print("{place}, the location of the {adj1} {verb} turtle goose, I love {adj2} turtle-geese like him")
+
+if storynum == 0:
+	print("from the journal of clornton the explorer: a {adj1} being sat {verb} in front of me, it was then I realized I was in{place}. This {adj2} land is home to many creatures.")
