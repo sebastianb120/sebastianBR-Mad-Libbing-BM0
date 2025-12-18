4d8395e81ee4d30e70fe7ddf47fb98a75757694a
sebastianb120 <sebastianb120@nycstudents.net>
2025-12-18 12:00:44 -0500

    Update texteditor.py

--- texteditor.py
+++ texteditor.py
@@ -6,6 +6,8 @@ root = Tk()
 root.title("text editor on BIDDY")
 root.geometry("300x300")
 
+custom_font = ("Helvetica", 16, "bold")
+
 text_area = scrolledtext.ScrolledText(root,
                                       wrap = WORD,
                                       width = 40,
@@ -13,5 +15,17 @@ text_area = scrolledtext.ScrolledText(root,
                                       font = ("Times New Roman",
                                               15))
 
+savename = ttk.Entry(root, width=20, font=("Helvetica", 12)) 
+save = ttk.Button(
+  root,
+  width = 1.5,
+  text="save")
+  #,command=savetext)
+save.place(x=25,y=175)
+
+openbutton = ttk.Button(root, width =1.5, text="open")
+openbutton.place(x = 225,y=175)
+
 text_area.pack()
+savename.pack()
 root.mainloop()
